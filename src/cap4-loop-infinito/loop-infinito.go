ae2243689ec942cef19df35f1adbc02ae457214b
Senio Caires <seniocaires@gmail.com>
2016-10-04 14:01:21 +0000

    loop-infinito

---
+++ src/cap4-loop-infinito/loop-infinito.go
@@ -0,0 +1,27 @@
+package main
+
+import(
+  "fmt"
+  "math/rand"
+  "time"
+)
+
+func main() {
+  rand.Seed(time.Now().UnixNano())
+  n := 0
+
+  for {
+    n++
+
+    i := rand.Intn(4200)
+    fmt.Println(i)
+
+    if i % 42 == 0 {
+      break
+    }
+  }
+
+  fmt.Printf("Saida apos %d iteracoes.\n", n)
+}
+
+// go run src/cap4-loop-infinito/loop-infinito.go
