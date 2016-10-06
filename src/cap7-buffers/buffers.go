46a7bd5a3aae9d85129a97107538187ad074573a
Senio Caires <seniocaires@gmail.com>
2016-10-06 00:32:04 +0000

    buffers

---
+++ src/cap7-buffers/buffers.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func produzir(c chan int) {
+  c <- 1
+  c <- 2
+  c <- 3
+
+  close(c)
+}
+
+func main() {
+  c := make(chan int, 3)
+  go produzir(c)
+
+  for valor := range c {
+    fmt.Println(valor)
+  }
+}
+
+// go run src/cap7-buffers/buffers.go
