92ff42cab55c4a910d03d3ac0d0f12d36d1afa97
Senio Caires <seniocaires@gmail.com>
2016-10-06 01:25:25 +0000

    timeout

---
+++ src/cap7-timeout/timeout.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+  "fmt"
+  "time"
+)
+
+func executar(c chan <- bool) {
+  time.Sleep(5 * time.Second)
+  c <- true
+}
+
+func main() {
+  c := make(chan bool, 1)
+  go executar(c)
+
+  fmt.Println("Esperando...")
+
+  fim := false
+
+  for !fim {
+    select {
+      case fim = <- c:
+        fmt.Println("Fim!")
+      case <- time.After(2 * time.Second):
+        fmt.Println("Timeout!")
+        fim = true
+    }
+  }
+}
+
+// go run src/cap7-timeout/timeout.go
